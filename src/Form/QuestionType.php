ab47d3a068c40890944b52eab9aaa639a9a524c2
JihedR <jihedr007@gmail.com>
2026-05-11 19:12:23 +0100

    Fix QuestionType class name to match filename

--- src/Form/QuestionType.php
+++ src/Form/QuestionType.php
@@ -12,7 +12,7 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
-class Question1Type extends AbstractType
+class QuestionType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
